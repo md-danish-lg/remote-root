3618fdccb40a87ac0fbbb6068cbbebada8b41643
md-danish-lg <dk665226@gmail.com>
2025-07-25 12:18:00 +0500

    add nav + header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='main-div'>HELLO WORLD</div>
+    <div className='main-div'>
+      <nav className='flex items-center justify-between px-2'>
+        <h1 className='text-4xl font-bold'><a href='#'>RemoteRoot</a></h1>
+        <a href="#" className='text-2xl font-bold '>Contact Us</a>
+
+      </nav>
+      
+      <section className="header mt-20">
+        <h1 className='text-6xl font-bold text-center '>RemoteRoot - Jobs Board</h1>
+
+      </section>
+
+
+
+    </div>
   )
 }
 
