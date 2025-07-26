5118096223edda5d72f76652b83382731797f7a1
md-danish-lg <dk665226@gmail.com>
2025-07-26 14:58:40 +0500

    show categories and location options when filter is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import Spinner from './components/Spinner';
-import Card from './components/Card'
-import Filter from './components/Filter';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Spinner from "./components/Spinner";
+import Card from "./components/Card";
+import Filter from "./components/Filter";
 
 const API_BASE_URL = `https://remotive.com/api/remote-jobs?limit=12`;
 
 const App = () => {
-
   const [job, setJob] = useState([]);
   const [loading, setloading] = useState(false);
-  const [showFilter, setshowFilter] = useState(false)
+  const [showFilter, setshowFilter] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async () => {
+    const endpoint = "https://remotive.com/api/remote-jobs/categories";
+    try {
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setCategories(data.jobs);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      return [];
+    }
+
+  };
+
+  
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   const handleClick = (e) => {
     e.preventDefault();
     const searchQuery = e.target.previousElementSibling.value;
     fetchJobs(searchQuery);
-    e.target.previousElementSibling.value = ''; // Clear the input field after search
+    e.target.previousElementSibling.value = ""; // Clear the input field after search
   };
 
   const handleFilter = () => {
     setshowFilter(!showFilter);
   };
 
-
-
-
-
-
-  
-  const fetchJobs = async (query = '') => {
+  const fetchJobs = async (query = "") => {
     setloading(true);
     setJob([]); // Clear previous jobs before fetching new ones
 
@@ -37,84 +52,83 @@ const App = () => {
       const endpoint = query ? `${API_BASE_URL}&search=${query}` : API_BASE_URL;
       const response = await fetch(endpoint);
 
-
-      if(!response.ok) {
-        throw new Error('Network response was not ok');
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      if(data.Respone === 'False') {
-        throw new Error('No jobs found');
+      if (data.Respone === "False") {
+        throw new Error("No jobs found");
       }
 
       setJob(data.jobs);
-      console.log(job);
-
-    }catch (error) {
-      console.error('Error fetching jobs:', error);
-    }finally {
+      
+    } catch (error) {
+      console.error("Error fetching jobs:", error);
+    } finally {
       setloading(false);
-    };
+    }
   };
-
 
   useEffect(() => {
     fetchJobs();
   }, []);
 
 
-
-
-
   return (
-    <div className='main-div '>
-      <nav className='flex items-center justify-between px-2 '>
-        <h1 className='text-4xl font-bold'><a href='#'>RemoteRoot</a></h1>
-        <a href="#" className='text-2xl font-bold '>Contact Us</a>
-
+    <div className="main-div ">
+      <nav className="flex items-center justify-between px-2 ">
+        <h1 className="text-4xl font-bold">
+          <a href="#">RemoteRoot</a>
+        </h1>
+        <a href="#" className="text-2xl font-bold ">
+          Contact Us
+        </a>
       </nav>
-      
+
       <section className="header mt-10">
-        <h1 className='text-6xl font-bold text-center '>RemoteRoot - Jobs Board</h1>
-         {/* input field for search */}
-        <div className='flex justify-center  gap-3 py-10'>
+        <h1 className="text-6xl font-bold text-center ">
+          RemoteRoot - Jobs Board
+        </h1>
+        {/* input field for search */}
+        <div className="flex justify-center  gap-3 py-10">
           <input
             type="text"
-            placeholder='Search for jobs...'
-            className='border-2 border-gray-300 rounded-lg p-2 w-1/2 py-3 focus:outline-none focus:border-blue-500 transition duration-200'
-          
+            placeholder="Search for jobs..."
+            className="border-2 border-gray-300 rounded-lg p-2 w-1/2 py-3 focus:outline-none focus:border-blue-500 transition duration-200"
           />
-          <button className='bg-blue-500 px-5 py-3 text-white cursor-pointer hover:bg-blue-800 duration-200 rounded-sm ' onClick={handleClick}>Search</button>
+          <button
+            className="bg-blue-500 px-5 py-3 text-white cursor-pointer hover:bg-blue-800 duration-200 rounded-sm "
+            onClick={handleClick}
+          >
+            Search
+          </button>
         </div>
-        
-        <div className='flex justify-start gap-0 items-center px-20 py-5'>
-          <button  className='cursor-pointer px-5 py-2 gap-1 flex items-center font-bold hover:scale-110 duration-200 transition-all' onClick={handleFilter}><img src='/filter-icon.svg' alt='filter-icon' className='h-5'/>Filter</button>
+
+        <div className="flex justify-start gap-0 items-center px-20 py-5">
+          <button
+            className="cursor-pointer px-5 py-2 gap-1 flex items-center font-bold hover:scale-110 duration-200 transition-all"
+            onClick={handleFilter}
+          >
+            <img src="/filter-icon.svg" alt="filter-icon" className="h-5" />
+            Filter
+          </button>
         </div>
-        {showFilter && (
-            <Filter handleFilter={handleFilter}/>
-        )}
-
-
+        {showFilter && <Filter handleFilter={handleFilter} categories={categories}/>}
       </section>
 
-      
-
-      <section className='mt-20mx-10'>
-
-        {loading ? (<Spinner />) : (
-          <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5   '>
+      <section className="mt-20mx-10">
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5   ">
             {job.map((job) => (
-            <Card key={job.id} job={job}/>
-            ))} 
-
-          </div> 
+              <Card key={job.id} job={job} />
+            ))}
+          </div>
         )}
-       
       </section>
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,22 +1,280 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
-const Filter = ({handleFilter}) => {
+const Filter = ({ handleFilter, categories }) => {
+  const country_list = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antigua &amp; Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia &amp; Herzegovina",
+    "Botswana",
+    "Brazil",
+    "British Virgin Islands",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Cape Verde",
+    "Cayman Islands",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "Congo",
+    "Cook Islands",
+    "Costa Rica",
+    "Cote D Ivoire",
+    "Croatia",
+    "Cruise Ship",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Estonia",
+    "Ethiopia",
+    "Falkland Islands",
+    "Faroe Islands",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Polynesia",
+    "French West Indies",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kuwait",
+    "Kyrgyz Republic",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macau",
+    "Macedonia",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Namibia",
+    "Nepal",
+    "Netherlands",
+    "Netherlands Antilles",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palestine",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Reunion",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Pierre &amp; Miquelon",
+    "Samoa",
+    "San Marino",
+    "Satellite",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "South Africa",
+    "South Korea",
+    "Spain",
+    "Sri Lanka",
+    "St Kitts &amp; Nevis",
+    "St Lucia",
+    "St Vincent",
+    "St. Lucia",
+    "Sudan",
+    "Suriname",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Timor L'Este",
+    "Togo",
+    "Tonga",
+    "Trinidad &amp; Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks &amp; Caicos",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "Uruguay",
+    "Uzbekistan",
+    "Venezuela",
+    "Vietnam",
+    "Virgin Islands (US)",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+  ];
+
+  
+
+
+  
+
+
+
+        
+        
+
+
+
   return (
     <>
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
-              <div className="bg-white rounded-lg shadow-xl p-8 max-w-lg w-full relative flex flex-col">
-                <button
-                  className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-[35px] font-bold mr-5"
-                  onClick={handleFilter}
-                >
-                  &times;
-                </button>
-
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
+        <div className="bg-white rounded-lg shadow-xl p-8 max-w-lg w-full relative flex flex-col">
+          <button
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-[35px] font-bold mr-5"
+            onClick={handleFilter}
+          >
+            &times;
+          </button>
+          <h3 className="text-2xl font-bold mb-4">Filter Jobs</h3>
+          <div className="flex flex-col gap-4">
+            <h1>Category</h1>
+            <div className="flex flex-wrap gap-2">
+                {categories.map((category, index) => (
+                    <button
+                    key={index}
+                    className="bg-gray-200 px-3 py-1 rounded hover:bg-gray-300 transition duration-200"
+                    onClick={() => handleFilter(category)}
+                    >
+                    {category.name}
+                    </button>
+                ))}
+              
                 
-                </div>
-              </div>
-    </>
-  )
-}
 
-export default Filter
+            </div>
+            
+              
+                
+                
+
+            
+
+            <h1>Location</h1>
+            <select name="country" className="border rounded px-2 py-1">
+                <option value="">Select Country</option>
+                {country_list.map((country, index) => (
+                  <option key={index} value={country}>{country}</option>
+                ))}
+            </select>
+
+            <a
+              href="#"
+              className="text-white font-bold  bg-blue-500 px-5 py-2 text-center rounded-md hover:bg-blue-800 transition-all duration-200 "
+            >
+              Apply
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
